075c8bd49421f0c801890df135ce2ec91c8473da
hritikCodes <ritikkumar887@gmail.com>
2021-12-27 12:39:36 +0530

    styled

--- sundaycodz/src/core/Header.jsx
+++ sundaycodz/src/core/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     return (
         <>
           <div className='bg-blue-200 p-4 rounded text-gray-600 m-2 flex flex-col items-center align-center sm:flex-row '>
-            <div className='self-auto sm:flex-1 '><Link to="/"><h1 className='text-6xl font-bold underline'>logo</h1></Link></div>
+            <div className='self-auto sm:flex-1 '><Link to="/"><button><h1 className='text-6xl font-bold underline'>Logo</h1></button></Link></div>
             <div className='self-auto sm:flex-1  my-auto'>Apps</div>
             <div className='self-auto sm:flex-1  my-auto'>Tutorials</div>
             <div className='self-auto sm:flex-1  my-auto'>Help</div>
