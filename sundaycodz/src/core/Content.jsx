01aa159ff95f57bf4c84a8abfeffcb517f4e8e4e
hritikCodes <ritikkumar887@gmail.com>
2021-12-27 14:05:37 +0530

    reveal nav scroll

--- sundaycodz/src/core/Content.jsx
+++ sundaycodz/src/core/Content.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Godseyeimg from '../resources/imgs/googlelens.png'
 import Googletransimg from '../resources/imgs/googletrans.png'
 import { Link } from 'react-router-dom'
+
 function Content() {
     return (
         <>
-            <main className="grid grid-cols-2 sm:grid-cols-4 justify-items-center gap-x-8 gap-y-10 px-5 py-2">
+            <main className="my-40 grid grid-cols-2 sm:grid-cols-4 justify-items-center gap-x-8 gap-y-10 px-5 py-2">
                     <Link to="/godseye">
                     <div className='h-15 w-40'>
                         <img src={Godseyeimg} alt="na" />
--- sundaycodz/src/core/Header.jsx
+++ sundaycodz/src/core/Header.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import "../resources/header.js"
 
 function Header() {
     return (
         <>
-          <div className='bg-blue-200 p-4 rounded text-gray-600 m-2 flex flex-col items-center align-center sm:flex-row '>
+          <header className='bg-blue-200 p-4 rounded text-gray-600 m-2 flex flex-col items-center align-center sm:flex-row '>
             <div className='self-auto sm:flex-1 '><Link to="/"><button><h1 className='text-6xl font-bold underline'>Logo</h1></button></Link></div>
             <div className='self-auto sm:flex-1  my-auto'>Apps</div>
             <div className='self-auto sm:flex-1  my-auto'>Tutorials</div>
             <div className='self-auto sm:flex-1  my-auto'>Help</div>
-          </div>  
+          </header>  
         </>
     )
 }
