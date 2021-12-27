e32602cc333d518110aebf218a9226bb08a47af0
abhishekdey4444 <abhishekdey4444@gmail.com>
2021-12-27 12:22:51 +0530

    webcam capture

--- sundaycodz/src/core/Godseye.jsx
+++ sundaycodz/src/core/Godseye.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import WebcamCapture from '../resources/webcam/WebcamCapture'
 
 function Godseye() {
     return (
         <div>
             <h1>Gods-Eye</h1>
+            <WebcamCapture />
         </div>
     )
 }
